67bd4ea032dacec26dd557f38af9ca02bbe66243
Dharun R <155513784+dharun36@users.noreply.github.com>
2025-08-13 14:31:42 +0530

    Time: 0 ms (100%), Space: 44.9 MB (78.14%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -3,15 +3,18 @@ class Solution {
         int left = 0;
         int right = nums.length -1;
         while(left < right){
-            if(nums[left] %2 ==0){
-                left++;
-            }
-            else{
+            if(nums[left]%2!=0 && nums[right]%2==0){
                 int t = nums[left];
                 nums[left] = nums[right];
                 nums[right] = t;
-                right --;
+                left++;
+                right--;
+                continue;
+
             }
+            if(nums[left]%2==0)left++;
+            if(nums[right]%2!=0)right--;
+            
         }
         return nums;
     }
