fdcd0deff76ce8cd9c521e3343630f4ebd1ebdb1
Dharun R <155513784+dharun36@users.noreply.github.com>
2025-09-17 12:12:59 +0530

    Time: 3 ms (38.65%), Space: 17.1 MB (61.77%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -3,43 +3,17 @@ public:
     int evalRPN(vector<string>& tokens) {
         stack<int> s;
         for(int i=0;i<tokens.size();i++){
-            if(tokens[i] == "+"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(a+b);
-
+            if(tokens[i]=="*" || tokens[i]=="/" || tokens[i]=="+" || tokens[i]=="-"){
+                int a = s.top(); s.pop();
+                int b = s.top(); s.pop();
+                if(tokens[i] == "+")s.push(a+b);
+                else if(tokens[i] == "*") s.push(a*b);
+                else if(tokens[i] == "-")s.push(b-a);
+                else if(tokens[i] == "/")s.push(b/a);
             }
-            else if(tokens[i] == "*"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(a*b);
-
-            }
-            else if(tokens[i] == "-"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b-a);
-
-            }
-            else if(tokens[i] == "/"){
-                int a = s.top();
-                s.pop();
-                int b = s.top();
-                s.pop();
-                s.push(b/a);
-
-            }
-            else{
-                s.push(stoi(tokens[i]));
-            }
-
+            else s.push(stoi(tokens[i]));
         }
+
         return s.top();
     }
 };
