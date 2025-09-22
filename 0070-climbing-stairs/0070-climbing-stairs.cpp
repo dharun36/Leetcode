1653862127cdadf8835fa3f5572a036a3ef7837a
Dharun R <155513784+dharun36@users.noreply.github.com>
2025-09-22 10:24:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (36.71%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,13 +1,20 @@
 class Solution {
 public:
-    int fib(int n,vector<int>&dp){
-        if(dp[n]!=-1)return dp[n];
-        if(n<=2) return dp[n] =n;
-        return dp[n] =  fib(n-1,dp) +  fib(n-2,dp);
-    }
+    // int fib(int n,vector<int>&dp){
+    //     if(dp[n]!=-1)return dp[n];
+    //     if(n<=2) return dp[n] =n;
+    //     return dp[n] =  fib(n-1,dp) +  fib(n-2,dp);
+    // }
+
     int climbStairs(int n) {
         
         vector<int>dp(n+1,-1);
-        return fib(n,dp);
+        if(n<=2) return n;
+        dp[1] =1;
+        dp[2] = 2;
+        for(int i = 3;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
 };
