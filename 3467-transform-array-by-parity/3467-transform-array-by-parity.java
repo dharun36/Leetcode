127ae4391710d674565a7ddb2cdb8a32c8b86511
Dharun R <155513784+dharun36@users.noreply.github.com>
2025-11-26 23:16:54 +0530

    Time: 1 ms (100%), Space: 46.5 MB (31.04%) - LeetHub

---
+++ 3467-transform-array-by-parity/3467-transform-array-by-parity.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] transformArray(int[] nums) {
+        int odd = 0;
+        int even = 0;
+
+        for(int i:nums){
+            if(i%2==0)even++;
+            else odd++;
+        }
+
+        for(int i = 0;i<even;i++){
+            nums[i]=0;
+
+        }
+
+        for(int i = even;i<nums.length;i++){
+            nums[i]=1;
+        }
+
+        return nums;
+    }
+}
